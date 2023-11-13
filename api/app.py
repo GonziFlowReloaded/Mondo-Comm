b409be8bd5395149b5fd89ffcdb99bfa6d6dfc94
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2023-11-13 17:05:20 -0300

    vercel anda xdios

---
+++ api/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException
+from emails.classes.c_mail import EmailSchema
+from emails.funcs.send_mail import enviar_correo
+import threading
+import os
+
+app = FastAPI()
+
+@app.get('/', tags=['Root'])
+def index():
+    return 'q onda mondonga'
+
+
+@app.post('/email_sender')
+def email_sender(Email: EmailSchema):
+    
+    print(Email)
+    enviar_correo(Email.destinatarios, Email.sujeto, Email.mensaje)
+    
+
+    return {"mensaje": "Correo enviado exitosamente"}
+
--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-from fastapi import FastAPI, HTTPException
-from emails.classes.c_mail import EmailSchema
-from emails.funcs.send_mail import enviar_correo
-import threading
-import os
+import uvicorn
 
-app = FastAPI()
-
-@app.get('/')
-def index():
-    return 'q onda mondonga'
-
-
-@app.post('/email_sender')
-def email_sender(Email: EmailSchema):
-    
-    print(Email)
-    enviar_correo(Email.destinatarios, Email.sujeto, Email.mensaje)
-    
-
-    return {"mensaje": "Correo enviado exitosamente"}
+if __name__ == "__main__":
+  uvicorn.run("api.app:app", host="0.0.0.0", port=8000, reload=True)
