00abbe88b7d5eef9214f20235e4ef6d9a9c68cbd
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2023-11-14 21:31:07 -0300

    ahora si

--- api/main.py
+++ api/main.py
@@ -1,7 +1,34 @@
 from flask import Flask, request
-from emails.funcs.adaptador import adapter
-
+from dotenv import load_dotenv
+import smtplib
+import os
+load_dotenv()
 app = Flask(__name__)
+
+
+def adapter(res):
+    destinatarios = res['destinatarios']
+    sujeto = res['sujeto']
+    mensaje = res['mensaje']
+    enviar_correo(destinatarios, sujeto, mensaje)
+    return {"message": "Mensaje enviado"}
+
+def enviar_correo(destinatarios, sujeto, mensaje):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        email = os.getenv('MONDO_MAIL')
+        password = os.getenv('MONDO_MAIL_PASS')
+        print(email, password)
+        smtp.login(email, password)
+
+        for destinatario in destinatarios:
+            asunto = sujeto
+            headers = f'From: {email}\nTo: {destinatario}\nSubject: {asunto}\n'
+            msg = f'{headers}\r\n{mensaje}'
+            smtp.sendmail(email, destinatario, msg)
+
 
 @app.route("/")
 def hello_world():
